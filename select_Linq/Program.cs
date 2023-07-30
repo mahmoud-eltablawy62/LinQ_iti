39313120206ba138cd979171d2960e53e515705b
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-30 14:13:53 +0300

    LinQ_iti

---
+++ select_Linq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace select_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "tom", "dick", "harry", "mary", "jay" };
+            var q1 = Names.Where((s) => s.Length == 3);
+            foreach (var s in q1)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
