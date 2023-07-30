a8acf18c34bbe957737ca8dd1cdc9529c4ff0a79
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-30 14:05:51 +0300

    linQ_iti

---
+++ multipul/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace multipul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+
+            {2,4,6,7,1,4,2,9,1};
+
+
+            var s_num = numbers.OrderBy(s => s);
+            var d_num = s_num.Distinct();
+            var mul = delegate (int x, int y) { return x * y; };
+            foreach (var q in d_num)
+            {
+                Console.WriteLine("number==> " + q + "multiply==>"+mul(q, q));
+            }
+
+        }
+    }
+}
