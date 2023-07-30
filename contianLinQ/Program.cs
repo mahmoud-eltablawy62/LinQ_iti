f660c408b59d81c8517caf9bb0ca58c6f83eb8cb
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-30 14:27:13 +0300

    LinQ_iti" git commit -m LinQ_iti git commit -m LinQ_iti"

---
+++ contianLinQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace contianLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "tom", "dick", "harry", "mary", "jay" };
+            var q0 = Names.OrderBy(x => x.Length);
+            var q1 = q0.Where((s) => s.Contains('a') );
+            foreach (var s in q1)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
