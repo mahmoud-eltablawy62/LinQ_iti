8d7034677a203644938b99ee8bbcc08a29d060df
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-30 13:37:42 +0300

    linQ(iti)

---
+++ linq(notRepeatandSorted_List)/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace linq_notRepeatandSorted_List_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> numbers = new List<int>()
+
+            {2,4,6,7,1,4,2,9,1};
+
+            
+            var s_num = numbers.OrderBy(s => s);
+            var d_num = s_num.Distinct();
+            foreach (var q in d_num ) {
+                Console.WriteLine(q);
+            }
+
+        }
+    }
+}
